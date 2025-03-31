5ef9549814fa8edd195e716a96f8ebfbc8211e78
odutradev <joao.vitornl@gmail.com>
2025-03-31 15:50:05 -0300

    feat: Configure footer component

---
+++ src/components/footer/styles.ts
@@ -0,0 +1,34 @@
+import { Box, Link, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export const FooterContainer = styled(Box)(({ theme }) => ({
+    backgroundColor: theme.palette.background.paper,
+    justifyContent: 'space-between',
+    boxShadow: theme.shadows[3],
+    padding: theme.spacing(2),
+    alignItems: 'center',
+    position: 'fixed',
+    display: 'flex',
+    bottom: 0,
+    right: 0,
+    left: 0
+}));
+
+export const FooterRight = styled(Box)(() => ({
+    alignItems: 'center',
+    display: 'flex',
+}));
+
+export const DevelopedByText = styled(Typography)(({ theme }) => ({
+    color: theme.palette.text.secondary,
+    fontSize: '0.875rem',
+    fontWeight: 400,
+}));
+
+export const FooterLink = styled(Link)(({ theme }) => ({
+    color: theme.palette.primary.main,
+    marginLeft: theme.spacing(1),
+    textDecoration: 'none',
+    fontSize: '0.875rem',
+    fontWeight: 'bold',
+}));
