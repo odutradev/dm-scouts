10ea6c12db37fd6c488481a13b82f88bb073c837
odutradev <joao.vitornl@gmail.com>
2025-03-31 14:06:44 -0300

    fix: Fix notfound moongose error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 import chalk from 'chalk';
 import path from 'path';
 
@@ -15,8 +14,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [
-      react(),
-      svgr()
+      react()
     ],
     resolve: {
       alias: {
