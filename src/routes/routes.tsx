5e0b4b48aa832371b122af2895b7a58db191abcb
odutradev <joao.vitornl@gmail.com>
2025-04-01 11:59:34 -0300

    feat: Configure routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,17 +1,24 @@
-import Main from "@pages/main";
+import { Navigate } from "react-router-dom";
+
 import NotFound from "@pages/notFound";
 import SignIn from "@pages/signin";
 import SignUp from "@pages/signup";
+import Main from "@pages/main";
+
+const InitialRoute = () => localStorage.getItem("token") != null ? <Navigate to="/dashboard" replace/> : <Navigate to="/signin" />;
 
 const routes = [
     {
         path: "/",
         privateRoute: false,
         routes: [
+            ["*", <Navigate to="/not-found" replace/>],
+            ['not-found', <NotFound />],
+            ['/dashboard', <Main />],
             ['/signin', <SignIn />],
             ['/signup', <SignUp />],
-            ['*', <NotFound />],
-            ['/', <Main />],
+            ['', <InitialRoute />],
+
         ]
     },
 ];
