ac1c35ce57da0ab35a7dccf2bd19383876071bba
dutra-pc <joao.vitornl@gmail.com>
2025-04-01 23:09:01 -0300

    refactor: Refactor layout component

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useTheme } from "@mui/material/styles";
+import { Container, Content } from "./styles";
+import { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+
 import Loading from "@components/loading";
 import Footer from "@components/footer";
-import { LayoutProps } from "./types";
-import { Container, Content } from "./styles";
-import MenuIcon from "@mui/icons-material/Menu";
-import Button from "@mui/material/Button";
-import MenuDrawer from "./components/menu";  // Importe o MenuDrawer
-import { useTheme } from "@mui/material/styles";
 
+import type { LayoutProps } from "./types";
+
+import MenuDrawer from "./components/menu";
 const Layout = ({ children, isLoading = false, showFooter = true, title = "Título Padrão" }: LayoutProps) => {
     const [menuOpen, setMenuOpen] = useState(false);
     const theme = useTheme();
@@ -29,14 +31,13 @@ const Layout = ({ children, isLoading = false, showFooter = true, title = "Títu
                     backgroundColor: theme.palette.action.disabledBackground, 
                     color: theme.palette.text.primary, 
                     '&:hover': { backgroundColor: theme.palette.action.hover },
-                    width: '200px',  // Largura maior do botão
+                    width: '200px',
                 }}
                 startIcon={<MenuIcon />}
             >
                 Menu
             </Button>
 
-            {/* Componente MenuDrawer agora independente */}
             <MenuDrawer 
                 menuOpen={menuOpen} 
                 setMenuOpen={setMenuOpen} 
