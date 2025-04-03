ff907970b9ff1b8461f972bb01310c04f74579bc
dutra-pc <joao.vitornl@gmail.com>
2025-04-02 21:13:30 -0300

    feat: Create admin config

---
+++ src/pages/adminConfig/index.tsx
@@ -0,0 +1,14 @@
+import UserNavbar from '@components/userNavbar';
+import Layout from '@components/layout';
+
+const AdminConfig = () => {
+
+  return (
+    <Layout title='Configurações'>
+      <UserNavbar />
+      
+    </Layout>
+  );
+};
+
+export default AdminConfig;
